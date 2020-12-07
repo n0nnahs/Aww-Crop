cb471fc5f82ca4741613caa5a6cc5bc58f6c2831
Tom Medvitz <tom@techelevator.com>
2020-12-07 12:27:07 -0500

    Initial commit

---
+++ vue/src/main.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router/index'
+import store from './store/index'
+import axios from 'axios'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
+Vue.config.productionTip = false
+
+axios.defaults.baseURL = process.env.VUE_APP_REMOTE_API;
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
