8c7a72f13c9a66fa57433c03bd95d5ed393f6bba
Kayla Mulheran <k.frances.mulheran@gmail.com>
2020-12-09 12:08:07 -0500

    welcome page is finished and responsive

--- vue/src/views/Welcome.vue
+++ vue/src/views/Welcome.vue
@@ -5,10 +5,10 @@
             <h1 id="heading">Aww Crop! The Garden Planning App</h1>
         <div id="pineapples">
             <div id="login">
-              <router-link style="color: #4e2409;" v-bind:to="{ name: 'login' }">Have an account already? Sign in.</router-link>
+              <router-link style="color: #4e2409;" v-bind:to="{ name: 'login' }">Have an account already? <span style="font-style:italic;font-weight:bold"> Sign in.</span></router-link>
             </div>
             <div id="register">
-              <router-link style="color: #4e2409;" v-bind:to="{ name: 'register' }">If not, click here to create one!</router-link>
+              <router-link style="color: #4e2409;" v-bind:to="{ name: 'register' }">If not, click <span style="font-style:italic;font-weight:bold">here</span> to create one!</router-link>
             </div>
         </div>
     </div>
@@ -74,7 +74,6 @@ export default {
   grid-area: shop;
   text-align: center;
 }
-
 #main-grid {
   display: grid;
   grid-template-columns:1fr;
@@ -84,31 +83,27 @@ export default {
     "footer";
 }
 #info-columns {
-    grid-area: info-columns;
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
-    grid-template-areas:
-            "plan track shop";
-    text-align: center;
+  grid-area: info-columns;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-areas:
+    "plan track shop";
+  text-align: center;
 }
-
 #header-image{
   width: 100%;
-  position: relative;
-  
+  position: relative; 
 }
-
 #heading{
-    width: 100%;
-    color: #4e2409;
-    font-size: 72px;
-    position: absolute;
-    top: 30%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    padding-bottom: 10px;
-    }
-
+  width: 100%;
+  color: #4e2409;
+  font-size: 72px;
+  position: absolute;
+  top: 30%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  padding-bottom: 10px;
+}
 #pineapples{
 width: 100%;
 font-size: 24px;
@@ -117,7 +112,6 @@ top: 50%;
 left: 50%;
 transform: translate(-50%, -50%);
 }
-
 h3{
   font-size: 32px;
   padding-bottom: 15 px;
@@ -129,5 +123,123 @@ h6{
   padding-top: 20px;
   font-size: 10 px;
   color:#b24747;
+}
+
+/*tablets*/
+@media (max-width: 800px){
+      #main-grid {
+        grid-template-columns: 100%;
+        grid-template-areas:
+            "attractive-header"
+            "info-columns"
+            "footer";
+      }
+      #info-columns {
+        grid-template-columns: 100%;
+        grid-template-areas:
+            "plan"
+            "track"
+            "shop";
+      }
+      #heading{
+        width: 100%;
+        color: #4e2409;
+        font-size: 36px;
+        position: absolute;
+        top: 10%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        padding-bottom: 10px;
+      }
+      #pineapples{
+        width: 100%;
+        font-size: 20px;
+        position: absolute;
+        top: 25%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+      }
+      #footer{
+        grid-area: footer;
+        text-align: center;
+      }
+}
+/*mobile*/
+@media (max-width: 450px){
+      #main-grid {
+        grid-template-columns: 100%;
+        grid-template-areas:
+            "attractive-header"
+            "info-columns"
+            "footer";
+      }
+      #info-columns {
+        grid-template-columns: 100%;
+        grid-template-areas:
+            "plan"
+            "track"
+            "shop";
+     }
+     #heading{
+        width: 100%;
+        color: #4e2409;
+        font-size: 36px;
+        position: absolute;
+        top: 10%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        padding-bottom: 10px;
+     }
+    #pineapples{
+        width: 100%;
+        font-size: 14px;
+        position: absolute;
+        top: 20%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+    #footer{
+        grid-area: footer;
+        text-align: center;
+    }  
+}
+/*extra small mobile*/
+@media (max-width: 325px){
+    #main-grid {
+        grid-template-columns: 100%;
+        grid-template-areas:
+            "attractive-header"
+            "info-columns"
+            "footer";
+    }
+    #info-columns {
+        grid-template-columns: 100%;
+        grid-template-areas:
+            "plan"
+            "track"
+            "shop";
+    }
+    #heading{
+        width: 100%;
+        color: #4e2409;
+        font-size: 22px;
+        position: absolute;
+        top: 10%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        padding-bottom: 10px;
+    }
+    #pineapples{
+        width: 100%;
+        font-size: 12px;
+        position: absolute;
+        top: 17%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+    #footer{
+        grid-area: footer;
+        text-align: center;
+    }
 }
 </style>
