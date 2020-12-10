06282b76205d340ffd4a4fc6d711f462d805cc44
Shannon Woods <45649247+n0nnahs@users.noreply.github.com>
2020-12-09 20:15:54 -0500

    backside stuff

--- java/src/main/java/com/techelevator/dao/PlotDAO.java
+++ java/src/main/java/com/techelevator/dao/PlotDAO.java
@@ -1,5 +1,16 @@
 package com.techelevator.dao;
 
+import java.util.List;
+
+import com.techelevator.model.Crop;
+import com.techelevator.model.Plot;
+
 public interface PlotDAO {
 
+	List<Plot> listAllForUser(int userId);
+	
+	List<Crop> listAllCropsInPlot(int plotId);
+	
+	boolean create(String name, int length, int width);
+	
 }
--- java/src/main/java/com/techelevator/dao/PlotSqlDAO.java
+++ java/src/main/java/com/techelevator/dao/PlotSqlDAO.java
@@ -1,5 +1,28 @@
 package com.techelevator.dao;
 
+import java.util.List;
+
+import com.techelevator.model.Crop;
+import com.techelevator.model.Plot;
+
 public class PlotSqlDAO implements PlotDAO {
+
+	@Override
+	public List<Plot> listAllForUser(int userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Crop> listAllCropsInPlot(int plotId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean create(String name, int length, int width) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
