6068eb89fec189c82892118ce88069f929aa46c6
Shannon Woods <45649247+n0nnahs@users.noreply.github.com>
2020-12-09 22:22:24 -0500

    merge issue

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -5,11 +5,7 @@
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
     </div>
     <header class="header" v-show="$route.name !== 'welcome'">
-<<<<<<< HEAD
-    </header> 
-=======
     </header>
->>>>>>> bb094f765770bce3b91b079e082534a69d69e010
     <router-view />
   </div>
 </template>
@@ -21,17 +17,11 @@
   font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
   height: 100vh;
 }
-<<<<<<< HEAD
-#nav{
-  background-color: #81e5fc;
-}
-=======
 
 #nav {
   background-color: #81e5fc;
 }
 
->>>>>>> bb094f765770bce3b91b079e082534a69d69e010
 header {
   background-image: url("./assets/veggies-fixed-clouds.jpg");
   background-repeat: repeat-x;
