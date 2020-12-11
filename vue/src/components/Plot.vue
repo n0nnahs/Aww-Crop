e3726970ccbc8c9e9d5fddcd577ce97a30529452
Sean Hunt <seanthunt@gmail.com>
2020-12-11 00:14:29 -0500

    Plot table

--- vue/src/components/Plot.vue
+++ vue/src/components/Plot.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
-      <table>
-        <tr>
-            <td>
-<button v-on:click="grid" />
+      <button v-on:click="grid">Button</button>
+      <table v-bind="grid" v-if="!isLoading">
+        <thead>Plot</thead>
+        <tr v-for="cropRow in this.plotGrid" v-bind:key="cropRow">
+            <td v-for="crop in cropRow" v-bind:key="crop">
+                {{crop}}
             </td>
         </tr>
       </table>
@@ -11,15 +13,18 @@
 </template>
 
 <script>
+import Crop from './Crop.vue';
 
 
 export default {
     data(){
         return{
+            isLoading: true,
             plotGrid:[[]]
         }
     },
     components:{
+        Crop
         
     },
     methods: {
@@ -27,8 +32,8 @@ export default {
             //take in height and create number of arrays in plotGrid
             //take in width and create length of arrays in plotGrid
             // assignCrops()
+            let width = this.$store.state.plotSize.width - 1;
             let height = this.$store.state.plotSize.height - 1;
-            let width = this.$store.state.plotSize.width;
             let row = [];
             for(let i = 0; i < width; i++){
                 row.push(0);
@@ -37,6 +42,7 @@ export default {
                 this.plotGrid.push(row)
             }
             this.assignCrops();
+            this.isLoading = false;
             console.log(this.plotGrid);
         },
         assignCrops(){
@@ -49,13 +55,35 @@ export default {
                 this.plotGrid[x][y] = (element.id);
             });
         }
+    },
+    created(){
+        this.grid();
+    },
+    computed:{
+
     }
 }
 
 </script>
 
 <style>
+div{
+    justify-content: center;
+}
 table{
     border-color: black;
+    table-layout: fixed;
+    justify-content: center;
+}
+td{
+    border: 1px solid black;
+}
+thead{
+    color:antiquewhite;
+    border: 1px solid black;
+}
+button{
+    color: white;
+    background-color: blue;
 }
 </style>
